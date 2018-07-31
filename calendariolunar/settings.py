1d42d2bba86a25767d9c69d0d640883b3f307783
mcueto <cueto@live.cl>
2018-07-31 02:09:59 -0400

    Add STATIC_ROOT to project settings

--- calendariolunar/settings.py
+++ calendariolunar/settings.py
@@ -114,8 +114,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
