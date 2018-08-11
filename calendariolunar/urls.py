bc4647fd44b1e7f85450756554d5972ee79efe24
mcueto <cueto@live.cl>
2018-08-10 22:17:54 -0400

    Change phases url to index

--- calendariolunar/urls.py
+++ calendariolunar/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('phases/', include('lunarphases.urls'))
+    path('/', include('lunarphases.urls'))
 ]
