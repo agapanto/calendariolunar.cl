02beb50226805d97c8d4cc639e8b8a66cbc1b9da
mcueto <cueto@live.cl>
2019-01-22 06:46:36 -0300

    Set es-cl as default language in setenvars.sh file

--- setenvvars.sh
+++ setenvvars.sh
@@ -1,5 +1,5 @@
 export DJANGO_SETTINGS_MODULE="calendariolunar.settings"
 export DEBUG=True
 export APP_HOST='127.0.0.1'
-export LANGUAGE_CODE='en-us'
+export LANGUAGE_CODE='es-cl'
 export TIME_ZONE='UTC'
