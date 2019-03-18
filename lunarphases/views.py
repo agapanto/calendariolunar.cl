ca57b31c3c95b196672962aa8cdc5ec0b6be956e
mcueto <cueto@live.cl>
2019-03-18 04:27:46 -0300

    Add following_lunar_phases to CurrentLunarPhaseView view & template

--- lunarphases/views.py
+++ lunarphases/views.py
@@ -10,6 +10,7 @@ from django.views import View
 from lunarphases import LunarPhase
 from .utils import (
     get_lunar_phase_tips,
+    get_following_lunar_phases,
 )
 
 # logger = logging.getLogger(__name__)
@@ -26,10 +27,14 @@ class CurrentLunarPhaseView(View):
         template = loader.get_template('index.html')
         lunar_phase = LunarPhase()
         lunar_phase_tips = get_lunar_phase_tips(lunar_phase)
+        following_lunar_phases = get_following_lunar_phases(
+            following_phases_count=4
+        )
 
         context = {
             'lunar_phase': lunar_phase,
             'tips': lunar_phase_tips,
+            'following_lunar_phases': following_lunar_phases,
         }
 
         return HttpResponse(template.render(context, request))
