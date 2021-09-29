5b40e0ce0091318c52980496e26ec695d2e6b5f1
Marcelo Cueto <cueto@live.cl>
2021-09-29 01:49:06 -0300

    Create CalendarioLunarSitemap #12

---
+++ lunarphases/sitemaps.py
@@ -0,0 +1,45 @@
+import datetime
+from django.contrib.sitemaps import Sitemap
+from django.utils.translation import gettext as _
+from django.urls import reverse
+
+
+class CalendarioLunarSitemap(Sitemap):
+    priority = 1
+    changefreq = 'daily'
+    i18n = True
+
+    def items(self):
+        today = datetime.datetime.now().date()
+
+        return [
+            {
+                'url': 'current_lunar_phase',
+                'kwargs': {}
+            },
+            {
+                'url': 'following_lunar_phases',
+                'kwargs': {}
+            },
+            {
+                'url': 'monthly_calendar',
+                'kwargs': {
+                    'year': today.year,
+                    'month': today.month,
+                }
+            },
+            {
+                'url': 'specific_lunar_phase',
+                'kwargs': {
+                    'year': today.year,
+                    'month': today.month,
+                    'day': today.day,
+                }
+            }
+        ]
+
+    def location(self, item):
+        return reverse(
+            item['url'],
+            kwargs=item['kwargs']
+        )
